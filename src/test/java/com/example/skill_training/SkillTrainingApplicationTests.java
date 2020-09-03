e1e3327dc8d1df0957c7594c0fce4531298ec299
zhihui1107 <759960833@qq.com>
2020-09-03 10:21:08 +0800

    Initial commit333

---
+++ src/test/java/com/example/skill_training/SkillTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skill_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
